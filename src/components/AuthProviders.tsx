bb24283327abb9b8f35a2315f56b1492a5559de4
HassanElwi <elwi.hassan@gmail.com>
2023-09-16 23:22:57 +0330

    AuthProviders has been created.

--- src/components/AuthProviders.tsx
+++ src/components/AuthProviders.tsx
@@ -1,3 +1,48 @@
+"use client";
+
+import { getProviders, signIn } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { Button } from "./ui-kits";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
+
 export default function AuthProviders() {
-  return <div></div>;
+  // Dictionary of providers
+  const [providers, setProviders] = useState<Providers | null>(null);
+  // #TODO remove console
+  console.log(providers);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+
+      // Update providers state
+      setProviders(res);
+    };
+
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+          <Button
+            key={i}
+            title="Sign In"
+            handleClick={() => signIn(provider?.id)}
+          />
+        ))}
+      </div>
+    );
+  }
 }
